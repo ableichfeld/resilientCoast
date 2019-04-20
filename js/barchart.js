45ac0426613140e405e775e6f45143c4c5fafcf2
ableichfeld <49157130+ableichfeld@users.noreply.github.com>
2019-04-20 17:04:33 -0400

    Graph work

--- js/barchart.js
+++ js/barchart.js
@@ -1,23 +1,33 @@
 var width = 750;
 var height = 500;
-var margin = {top: 25, left: 25, right: 25, bottom: 25};
+var margin = {top: 25, left: 75, right: 25, bottom: 75};
 
 var svg = d3.select("#barchart")
   .append("svg")
   .attr("width",width)
   .attr("height", height);
 
-  var data1 = [
-    {year: 2020, rise: 1},
-    {year: 2030, rise: 3},
-    {year: 2040, rise: 5},
-    {year: 2050, rise: 11},
-    {year: 2060, rise: 18},
-    {year: 2070, rise: 27},
+  var set1 = [
+    {year: 2030, rise: 6.0},
+    {year: 2050, rise: 13.2},
+    {year: 2070, rise: 26.4}
   ]
 
+  var set2 = [
+    {year: 2030, rise: 6.0},
+    {year: 2050, rise: 12.0},
+    {year: 2070, rise: 22.8}
+  ]
+
+  var set3 = [
+    {year: 2030, rise: 6.0},
+    {year: 2050, rise: 12.0},
+    {year: 2070, rise: 20.4}
+  ]
+
+
 var xScale = d3.scaleBand()
-  .domain(["2020","2030","2040","2050","2060","2070"])
+  .domain(["2030","2050","2070"])
   .rangeRound([margin.left, width-margin.right])
   .padding(0.5);
 
@@ -33,19 +43,47 @@ var yAxis = svg.append("g")
   .attr("transform",`translate(${margin.left},0)`)
   .call(d3.axisLeft().scale(yScale));
 
+var tooltip = d3.select("body")
+  .append("div")
+  .attr("class", "tooltip")
+  .style("position", "absolute")
+  .style("z-index", "10")
+  .style("visibility", "hidden");
+
+
 var bar = svg.selectAll("rect")
-  .data(data1)
+  .data(set2)
   .enter()
   .append("rect")
     .attr("x",function(d) { return xScale(d.year); })
     .attr("y", function(d) { return yScale(d.rise); })
     .attr("width",xScale.bandwidth())
     .attr("height", function(d) { return height - margin.bottom - yScale(d.rise); })
-    .attr("fill","steelblue")
-    .on("mouseover", function() {
-      d3.select(this)
-        .attr("fill","red");
-    }).on("mouseout", function() {
+    .attr("fill","#33cccc")
+    .on("mouseover", function(d) {
       d3.select(this)
         .attr("fill","steelblue");
-    })
+      tooltip.style("visibility", "visible")
+        .text(`Estimated sea level rise: ${d.rise} inches`);
+    }).on("mousemove", function(){return tooltip.style("top", (event.pageY-10)+"px").style("left",(event.pageX+10)+"px");})
+    .on("mouseout", function() {
+      d3.select(this)
+        .attr("fill","#33cccc")
+      return tooltip.style("visibility", "hidden");
+    });
+
+
+var xLabel = svg.append("text")
+  .attr("class", "axisLabel")
+  .attr("x", width/2)
+  .attr("y", height-20)
+  .attr("text-anchor", "middle")
+  .text("Year");
+
+var yLabel = svg.append("text")
+  .attr("class", "axisLabel")
+  .attr("transform", "rotate(-90)")
+  .attr("y", 20)
+  .attr("x", -height/2  )
+  .attr("text-anchor", "middle")
+  .text("Predicted sea level rise (inches)");
